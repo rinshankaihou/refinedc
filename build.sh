9a1d53864a9006ed2e8b64c56db51bdbccdff25a
Michael Sammler <noreply@sammler.me>
2020-11-25 08:25:20 +0100

    added build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+export TIMECMD="time -f \t%es\t%P"
+
+if [[ "${1##*.}" == "c" ]]; then
+    dune exec -- refinedc check --no-build "$1"
+    cd "$(dirname $1)/proofs/$(basename $1 .c)"
+    dune build --display short
+else
+    dune build --display short "$@"
+fi
