743e313b7e8ff07833923a59a3d3ab3f4b51d2d1
Michael Sammler <noreply@sammler.me>
2020-12-02 14:45:22 +0100

    add missing proofs

---
+++ tutorial/proofs/t06_struct/generated_proof_argtest.v
@@ -0,0 +1,27 @@
+From refinedc.typing Require Import typing.
+From refinedc.tutorial.t06_struct Require Import generated_code.
+From refinedc.tutorial.t06_struct Require Import generated_spec.
+Set Default Proof Using "Type".
+
+(* Generated from [tutorial/t06_struct.c]. *)
+Section proof_argtest.
+  Context `{!typeG Σ} `{!globalG Σ}.
+
+  (* Typing proof for [argtest]. *)
+  Lemma type_argtest (blue getblue : loc) :
+    blue ◁ᵥ blue @ function_ptr type_of_blue -∗
+    getblue ◁ᵥ getblue @ function_ptr type_of_getblue -∗
+    typed_function (impl_argtest blue getblue) type_of_argtest.
+  Proof.
+    start_function "argtest" ([]).
+    split_blocks ((
+      ∅
+    )%I : gmap label (iProp Σ)) ((
+      ∅
+    )%I : gmap label (iProp Σ)).
+    - repeat liRStep; liShow.
+      all: print_typesystem_goal "argtest" "#0".
+    Unshelve. all: sidecond_hook; prepare_sideconditions; normalize_and_simpl_goal; try solve_goal; unsolved_sidecond_hook.
+    all: print_sidecondition_goal "argtest".
+  Qed.
+End proof_argtest.
---
+++ tutorial/proofs/t06_struct/generated_proof_getblue.v
@@ -0,0 +1,25 @@
+From refinedc.typing Require Import typing.
+From refinedc.tutorial.t06_struct Require Import generated_code.
+From refinedc.tutorial.t06_struct Require Import generated_spec.
+Set Default Proof Using "Type".
+
+(* Generated from [tutorial/t06_struct.c]. *)
+Section proof_getblue.
+  Context `{!typeG Σ} `{!globalG Σ}.
+
+  (* Typing proof for [getblue]. *)
+  Lemma type_getblue :
+    ⊢ typed_function impl_getblue type_of_getblue.
+  Proof.
+    start_function "getblue" ([[r g] b]) => arg_b.
+    split_blocks ((
+      ∅
+    )%I : gmap label (iProp Σ)) ((
+      ∅
+    )%I : gmap label (iProp Σ)).
+    - repeat liRStep; liShow.
+      all: print_typesystem_goal "getblue" "#0".
+    Unshelve. all: sidecond_hook; prepare_sideconditions; normalize_and_simpl_goal; try solve_goal; unsolved_sidecond_hook.
+    all: print_sidecondition_goal "getblue".
+  Qed.
+End proof_getblue.
