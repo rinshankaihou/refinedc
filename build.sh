1838d2f69bc63699e59c668fad36216c5d3ad29f
Michael Sammler <noreply@sammler.me>
2022-03-04 19:44:53 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -8,9 +8,19 @@ if env time -f "\t%es\t%P" echo >/dev/null 2>/dev/null; then
 fi
 
 if [[ "${1##*.}" == "c" ]]; then
-    dune exec -- refinedc check "$1"
+    opam exec -- dune exec -- refinedc check "$1"
     cd "$(dirname $1)/proofs/$(basename $1 .c)"
-    dune build --display short
+    if [[ -f "generated_proof_$2.v" ]]; then
+        echo "Building" "$(pwd)/generated_proof_$2.vo"
+        opam exec -- dune build --display short "generated_proof_$2.vo"
+    else
+        echo "Building" "$1"
+        opam exec -- dune build --display short
+    fi
+else if [[ "${1##*.}" == "v" ]]; then
+    echo "Building" "$1o"
+    opam exec -- dune build --display short $(realpath --relative-to=. "$1o")
 else
-    make
-fi
+    echo "Running make"
+    opam exec -- make
+fi fi
